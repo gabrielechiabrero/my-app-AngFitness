f4d7a9db9051e92c44d318c7d2e59429878322b2
Chiabrero Gabriele <gabriele.chiabrero@edu.itspiemonte.it>
2025-01-21 20:47:14 +0100

    aggiunta del file per la gestione delle rotte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CorsiComponent } from './corsi/corsi.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {path:'', component: HomeComponent},
+
+    {path: 'corsi', component:CorsiComponent},
+
+];
