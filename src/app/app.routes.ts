eb73e33dfbd8c13ad56c6d6f5f4b83e076c6fe34
Chiabrero Gabriele <gabriele.chiabrero@edu.itspiemonte.it>
2025-01-22 00:09:27 +0100

    aggiunta la route per la pagine chi siamo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CorsiComponent } from './corsi/corsi.component';
+import { ChisiamoComponent } from './chisiamo/chisiamo.component';
 
 export const routes: Routes = [
     
     {path:'', component: HomeComponent},
 
     {path: 'corsi', component:CorsiComponent},
+
+    {path: 'chisiamo', component:ChisiamoComponent}
 
 ];
