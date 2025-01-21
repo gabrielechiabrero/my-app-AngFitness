56b723b83914c111e5e33af609af9cda6ac1b1c9
Chiabrero Gabriele <gabriele.chiabrero@edu.itspiemonte.it>
2025-01-21 20:49:11 +0100

    creazione del component home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CorsiService } from '../corsi/corsi.service';
+import { CorsoComponent } from '../corsi/corso/corso.component';
+import { Corsi } from '../corsi/corsi.model';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [CorsoComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent implements OnInit {
+
+  private corsiService = inject(CorsiService);
+
+  corsi = this.corsiService.loadedCorsiPreferiti;
+
+  ngOnInit(): void {
+      this.corsiService.loadCorsiPreferiti()
+        .subscribe({
+            complete: () => {
+              console.log("Caricamento completato")
+            }
+        })
+  }
+
+  removeCorso(corso: Corsi){
+    this.corsiService.deleteUser(corso).subscribe({
+      next: (resData=>{
+        console.log("Stai eliminando", resData);
+      })
+    })
+  }
+}
