f7728df3c78611fab616ef5d4a36966b6fc67fa1
Chiabrero Gabriele <gabriele.chiabrero@edu.itspiemonte.it>
2025-01-21 22:21:29 +0100

    modifica del component corso

--- src/app/corsi/corso/corso.component.ts
+++ src/app/corsi/corso/corso.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, Input} from '@angular/core';
+import { Component, input, OnInit} from '@angular/core';
 import { Corsi } from '../corsi.model';
+import { CorsiService } from '../corsi.service';
 
 @Component({
   selector: 'app-corso',
@@ -10,6 +11,31 @@ import { Corsi } from '../corsi.model';
 export class CorsoComponent {
 
   corso = input.required<Corsi>();
+  
+  corsi: Corsi[] = [];
+  corsiPreferiti: Corsi[] = [];
+  constructor(private corsiService: CorsiService) {}
 
-    
+  ngOnInit() {
+    // Carica tutti i corsi
+    this.corsiService.loadCorsi().subscribe((data) => {
+      this.corsi = data;
+    });
+
+    // Carica i preferiti
+    this.corsiService.loadCorsiPreferiti().subscribe(() => {
+      this.corsiPreferiti = this.corsiService.loadedCorsiPreferiti();
+    });
+  }
+
+  preferito(corso: Corsi) {
+    this.corsiService.onPreferito(corso).subscribe(() => {
+      // Aggiorna i corsi preferiti dopo l'operazione
+      this.corsiPreferiti = this.corsiService.loadedCorsiPreferiti();
+    });
+  }
+
+  isPreferito(corso: Corsi): boolean {
+    return this.corsiPreferiti.some((c) => c.id === corso.id);
+  }
 }
