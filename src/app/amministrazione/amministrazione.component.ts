c783969c43592bcd637a300637aa5d6e8de94810
Chiabrero Gabriele <gabriele.chiabrero@edu.itspiemonte.it>
2025-01-23 10:30:57 +0100

    creazione di un component per la pagina amministrazione

---
+++ src/app/amministrazione/amministrazione.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amministrazione',
+  imports: [],
+  templateUrl: './amministrazione.component.html',
+  styleUrl: './amministrazione.component.css'
+})
+export class AmministrazioneComponent {
+
+}
