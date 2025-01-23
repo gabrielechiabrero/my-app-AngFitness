e3354dd41007bf1a73992bd29b64ecccefdb629e
Chiabrero Gabriele <gabriele.chiabrero@edu.itspiemonte.it>
2025-01-23 10:30:31 +0100

    aggiunto un controllo se non ci sono corsi viene creato un bottone per andare alla pagina corsi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CorsiService } from '../corsi/corsi.service';
 import { CorsoComponent } from '../corsi/corso/corso.component';
 import { Corsi } from '../corsi/corsi.model';
-
+import { Router } from '@angular/router';
+import { PrenotazioneComponent } from "../prenotazione/prenotazione.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CorsoComponent],
+  imports: [CorsoComponent, PrenotazioneComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -14,16 +15,20 @@ import { Corsi } from '../corsi/corsi.model';
 export class HomeComponent implements OnInit {
 
   private corsiService = inject(CorsiService);
-
   corsi = this.corsiService.loadedCorsiPreferiti;
+  corsoSelezionato!: Corsi;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-      this.corsiService.loadCorsiPreferiti()
+      console.log(this.corsiService.loadCorsiPreferiti()
         .subscribe({
             complete: () => {
               console.log("Caricamento completato")
             }
-        })
+        }));
+
+        
   }
 
   removeCorso(corso: Corsi){
@@ -32,5 +37,13 @@ export class HomeComponent implements OnInit {
         console.log("Stai eliminando", resData);
       })
     })
+  }
+
+  moveToCorsi(){
+    this.router.navigate(['/corsi']);
+  }
+
+  openModal(corso: Corsi){
+    this.corsoSelezionato = corso;
   }
 }
