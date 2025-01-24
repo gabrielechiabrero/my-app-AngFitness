94b793a13ae2521f02ec564a4013b18d3aac4287
Chiabrero Gabriele <gabriele.chiabrero@edu.itspiemonte.it>
2025-01-24 11:37:09 +0100

    aggiunto gestione delle prenotazioni in amministrazione

--- src/app/amministrazione/amministrazione.component.ts
+++ src/app/amministrazione/amministrazione.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { PrenotazioneService } from '../prenotazione/prenotazione.service';
+import { Prenotazione } from '../prenotazione/prenotazione.model';
+import { CorsiService } from '../corsi/corsi.service';
+import { Corsi } from '../corsi/corsi.model';
 
 @Component({
   selector: 'app-amministrazione',
@@ -6,6 +10,51 @@ import { Component } from '@angular/core';
   templateUrl: './amministrazione.component.html',
   styleUrl: './amministrazione.component.css'
 })
-export class AmministrazioneComponent {
+export class AmministrazioneComponent implements OnInit {
 
+  corsiPren = signal<Prenotazione[] | undefined>(undefined);
+  corsi = signal<Corsi[] | undefined>(undefined);
+  private destroyRef = inject(DestroyRef);
+  private prenotazioneService = inject(PrenotazioneService);
+  private corsiService = inject(CorsiService);
+
+  ngOnInit(): void {
+        
+    const subscription = this.prenotazioneService.loadPrenotazioni()
+        .subscribe({
+          next: (resData) => {
+            this.corsiPren.set(resData);
+          }, 
+        })
+
+    const subscription2 = this.corsiService.loadCorsi()
+    .subscribe({
+      next: (data) => {
+        this.corsi.set(data);
+      }, 
+    })
+    
+    this.destroyRef.onDestroy(()=>{
+          subscription.unsubscribe();
+        }
+        )
+    
+    this.destroyRef.onDestroy(()=>{
+      subscription2.unsubscribe();
+    }
+    )
+  }
+
+  onDelete(corso: Corsi){
+    this.corsiService.deleteCorso(corso).subscribe({
+      next: (resData=>{
+        console.log("Stai eliminando", resData);
+      })
+    })
+    this.corsiService.deletePreferito(corso).subscribe({
+      next: (resData=>{
+        console.log("Stai eliminando", resData);
+      })
+    })
+  }
 }
